e391b429b2b00ebab6c729102678141af13ab4c9
Ahmed Amer <amer73090@gmail.com>
2025-12-01 21:33:12 +0200

    production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api' // Change this to your backend API URL
-  
+  apiUrl: 'https://back-end-elderly-care-management-production.up.railway.app/api' 
 };
 
